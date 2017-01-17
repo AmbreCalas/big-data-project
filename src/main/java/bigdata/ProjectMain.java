089428c637b4c1e011027694d1c8cd07576ee5c5
RomainAymes <aymes.ro@gmail.com>
2017-01-17 12:31:53 +0100

    Partie 1 récupérations de tous les fichiers

--- src/main/java/bigdata/ProjectMain.java
+++ src/main/java/bigdata/ProjectMain.java
@@ -1,6 +1,9 @@
 package bigdata;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -11,6 +14,8 @@ import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
@@ -39,9 +44,21 @@ public class ProjectMain {
 	    job.setOutputKeyClass(Text.class);
 	    job.setOutputValueClass(SumUpWritable.class);
 	    job.setOutputFormatClass(TextOutputFormat.class);
-	    job.setInputFormatClass(RandomPointInputFormat.class);  
 	    FileInputFormat.addInputPath(job, new Path(inputFile));
 	    FileOutputFormat.setOutputPath(job, new Path(outputFile));
+	    
+	    job.setNumReduceTasks(1);
+	    DirectoryReader dirReader = new DirectoryReader(inputFile);
+	    ArrayList<String> files = dirReader.getFileList(); //explorer.listDirectory();
+	    for(String current: files){
+	    	MultipleInputs.addInputPath(job, new Path(inputFile + current), TextInputFormat.class, SumUpMapper.class);
+	    	
+	    	BufferedReader brFile = new BufferedReader(new FileReader(inputFile + current));
+	        String firstLine = brFile .readLine();
+	        conf.set(current, firstLine);
+	        brFile.close();
+	    }
+	    
 	    System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 	
