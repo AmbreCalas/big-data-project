0c1b3d6a58dac42eca8763016765a9a1c6154c0b
RomainAymes <aymes.ro@gmail.com>
2017-01-30 14:11:24 +0100

    Code partie 1 fini

--- src/main/java/bigdata/FilesMapReduce.java
+++ src/main/java/bigdata/FilesMapReduce.java
@@ -1,118 +1,187 @@
 package bigdata;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 public class FilesMapReduce {
 
 	// CLASS MAPPER
-		public static class FilesMapper extends Mapper<Object, Text, Text, Text>{
+		public static class FilesMapper extends Mapper<Object, Text, Text, CleanWritable>{
 			// map function
+			private static final String[] CATEGORIES = {"VETERAN","SENIOR","JUNIOR","MINIME","CADET","ESPOIR","CADETTE","BENJAMIN","HANDISPORT"};
 			public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 				// split lines to get different informations
 				
-				/*String[] parts = value.toString().split(";");
-				Text raceKey = new Text("");
-				
-				// TO DOOOOOO : TREAT SPEED
-				double speed = 0;
-				//if(parts[2].charAt(0) >= '0' || parts[5].charAt(0) <= '9') {
-					//speed = 
-				//}
+				String[] parts = value.toString().split(";");
+				String range = parts[0];
+				String time = "";
 				String category = "";
+				String name = "";
+				String team = "";
+				String newKey = "";
+				boolean canBeWritten = true;
+				int afterName = 0;
+				FileSplit file = (FileSplit) context.getInputSplit();
+				String[] fileNameSplit = file.getPath().getName().toString().split("/");
+				String fileName = fileNameSplit[fileNameSplit.length - 1];
+				fileName = fileName.substring(0,fileName.length() - 5);
 				
-				
-				// First name and last name are not in the same part
-				if(parts.length == 9) {
-					category = treatCategory(parts[8]);
-					raceKey = generateKey(parts[2], category);
-					/*context.write(raceKey, new SumUpWritable(category, parts[2], "1", parts[7], 
-							parts[7], parts[7], Double.toString(speed)));
-			
-				} 
-				// First name and last name are in the same part
-				else if(parts.length == 8) {
-					// Handle weird category place
-					if(parts[5].charAt(0) >= '0' || parts[5].charAt(0) <= '9') {
-						category = treatCategory(parts[5]);
-						raceKey = generateKey(parts[2], category);	
-						/*context.write(raceKey, new SumUpWritable(category, parts[2], "1", parts[6], 
-								parts[6], parts[6], Double.toString(speed)));
-					} 
-					// Category is at the right place
+				// We check if the lastname and the firstname are together or not 
+				if (areTogetherLastFirst(parts[0])) {
+						name = parts[1].toUpperCase();
+						afterName = 2;
+				}
+				else {
+						name = parts[1].toUpperCase() + " " + parts[2].toUpperCase();
+						afterName = 3;
+				}
+				for (int i = afterName; i < parts.length; i++) {
+					 if (isCategory(parts[i].toUpperCase())) {
+						 category = parts[i].toUpperCase();
+					 }
+					else if (isTime(parts[parts.length - 2])) {
+						time = adaptTime(parts[i]);
+					}
 					else {
-						category = treatCategory(parts[7]);
-						raceKey = generateKey(parts[2], category);
-						/*context.write(raceKey, new SumUpWritable(category, parts[2], "1", parts[6], 
-								parts[6], parts[6], Double.toString(speed)));
+						team = parts[i].toUpperCase(); 
 					}
-					*/
-				context.write(new Text(key.toString()), new Text(value));
-			}
-			
-			// Key generator
-			public Text generateKey(String distance, String category) {
-				String raceKey = distance + ";" + category;
-				Text toReturn = new Text(raceKey);
-				return toReturn;
-			}
-			
-			public String treatCategory(String category) {
-				String toReturn = "";
-				if(category.charAt(0) >= '0' || category.charAt(0) <= '9') {
-					String[] categoryParts = category.split(" ");
-					if(categoryParts.length == 2) {	
-						toReturn = categoryParts[1];
-					} else if(categoryParts.length == 3) {
-						toReturn = categoryParts[1] + categoryParts[2];
-					} else if(categoryParts.length == 4) {
-						toReturn = categoryParts[1] + categoryParts[2] + categoryParts[3];
-					}
-				return toReturn;
-				} else {
-					return category;
+				} 
+				
+				if (isKey(fileName)) {
+					newKey = generateKey(fileName);
+				}
+				String [] elements = {newKey,time,category,name,team};
+				canBeWritten = isNoOneEmpty(elements);
+				if (canBeWritten) {
+					CleanWritable clean = new CleanWritable(time,category,range,name,team); 
+					context.write(new Text(newKey), clean);
 				}
 			}
-		}
-	  
+			
+			// Auxiliar functions
+			
+			public boolean areTogetherLastFirst(String name) {
+				if (name.split(" ").length > 1) {
+					return true;
+				}
+				return false;
+			}
+			
+			public boolean isCategory(String category) {
+				 Pattern pattern;
+				 Matcher matcher;
+				 for (String validCategory: CATEGORIES) {
+					 pattern = Pattern.compile(validCategory);
+					 matcher = pattern.matcher(category);
+					 if (matcher.find()) {
+						 return true;
+					 }
+				 }
+				 return false;
+			}
+			
+			public boolean isTime(String time) {
+				if(time.matches("d{2,2}\\S\\d{2,2}")) {
+					return true;
+				}
+				else if (time.matches("\\d{1,2}\\S\\d{2,2}\\S\\d{2,2}\\S")) {
+					return true;
+				}
+				else if (time.matches("\\d{1,2}\\S\\d{2,2}\\S\\S\\d{2,2}\\S")) {
+					return true;
+				}
+				else if (time.matches("\\d{1,2}\\S\\d{2,2}\\S\\d{2,2}")) {
+					return true;
+				}
+				else if (time.matches("\\d{1,2}\\S\\S\\d{2,2}\\S\\d{2,2}")) {
+					return true;
+				}
+				else if (time.matches("d{2,2}\\S\\d{2,2}\\S")) {
+					return true;
+				}
+				return false;
+			}
+			
+			public String adaptTime(String time) {
+				time = time.replaceAll("[^\\d]",":");
+				time = time.split("\\.")[0];
+				if (time.charAt(time.length() - 1) == ':') {
+					time = time.substring(0, time.length() -2);
+				}
+				time = time.replace("::",":");
+				String[] timeSplit = time.split(":");
+				if (timeSplit.length == 1) {
+					time = "00:" + timeSplit[0] + ":" + "00";
+				}
+				else if (timeSplit.length == 2) {
+					time = "00:" + time;
+				}
+				else if (timeSplit.length == 3) {
+					if (timeSplit[0].length() == 1) {
+						time = "0" + time;
+					}
+				}
+				return time;
+			}
+			
+			public boolean isKey(String fileName) {
+				if (fileName.matches("\\d{1,2}\\S{2,30}\\d{1,2}\\S{0,2}")) {
+					return true;
+				}
+				return false;
+			}
+			
+			public String generateKey(String key) {
+				String years = "";
+				String city = "";
+				String distance = "";
+				if (key.matches("\\d{2,2}\\S{2,30}\\d{2,2}")) {
+					years = key.substring(0,1);
+					city = key.substring(2,key.length() - 3);
+					distance = key.substring(key.length() - 2,key.length() - 1);
+				}
+				else if (key.matches("\\d{1,1}\\S{2,30}\\d{2,2}")) {
+					years = key.substring(0,0);
+					city = key.substring(2,key.length() - 3);
+					distance = key.substring(key.length() - 2,key.length() - 1);
+				}
+				else if (key.matches("\\d{1,1}\\S{2,30}\\d{1,1}")) {
+					years = key.substring(0,0);
+					city = key.substring(1,key.length() - 2);
+					distance = key.substring(key.length() - 1,key.length() - 1);
+				}
+				else if (key.matches("\\d{2,2}\\S{2,30}\\d{1,1}")) {
+					years = key.substring(0,1);
+					city = key.substring(2,key.length() - 2);
+					distance = key.substring(key.length() - 1,key.length() - 1);
+				}
+				
+				return city + ";" + years + ";" + distance + ";";
+ 			}
+			
+			public boolean isNoOneEmpty(String[] elements) {
+				for (String el: elements) {
+					if (el.isEmpty()) {
+						return false;
+					}
+				}
+				return true;
+			}
+			
+	  }
 	  
 		// CLASS REDUCER
-		public static class FilesReducer extends Reducer<Text,Text,Text,Text> {
-			public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-				/*String[] keyParts = key.toString().split(";");
-				String category = keyParts[0];
-				String distance = keyParts[1];
-				
-				long nbPax = 0;
-				long minTimeInSeconds = 10*3600;
-				long maxTimeInSeconds = 0;
-				long avgTimeInSeconds = 0;
-				double speedSum = 0;
-				
-				/*for(SumUpWritable value : values) {
-					nbPax++;
-					avgTimeInSeconds += Long.parseLong(value.avgTime);
-					speedSum += Double.parseDouble(value.avgSpeed);
-					if(minTimeInSeconds > Long.parseLong(value.minTime)) {
-						minTimeInSeconds = Long.parseLong(value.minTime);
-					} 
-					if(maxTimeInSeconds > Long.parseLong(value.maxTime)) {
-						maxTimeInSeconds = Long.parseLong(value.maxTime);
-					}
-				}
-				double avgSpeed = speedSum / nbPax;
-				*/
-				/*SumUpWritable toReturn = new SumUpWritable(category, distance, Long.toString(nbPax), 
-						Long.toString(minTimeInSeconds), Long.toString(maxTimeInSeconds), 
-						Long.toString(avgTimeInSeconds), Double.toString(avgSpeed));*/
-				
-				//context.write(key, toReturn);*/
-				for (Text line: values) {
+		public static class FilesReducer extends Reducer<Text,CleanWritable,Text,CleanWritable> {
+			public void reduce(Text key, Iterable<CleanWritable> values, Context context) throws IOException, InterruptedException {
+				for (CleanWritable line: values) {
 					context.write(key, line);
 				}
 				
